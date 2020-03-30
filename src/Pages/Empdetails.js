1d4dd27e4ab169fad086972b39170393801df428
manojisking <manojprabaharan5@gmail.com>
2020-03-30 22:40:44 +0530

    changed employee details

--- src/Pages/Empdetails.js
+++ src/Pages/Empdetails.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Roshan from "/SGIC/SGIC-LMS/src/EmpPhotos/Roshan.jpg";
+
 
 function Empdetails() {
     return (
@@ -30,7 +30,7 @@ function Empdetails() {
           <div class="col-md-6">
             <div class="d-flex flex-row border rounded">
                 <div class="p-0 w-25">
-                    <img src={Roshan} class="img-thumbnail border-0" />
+                    <img src="https://c1.staticflickr.com/3/2862/12328317524_18e52b5972_k.jpg" class="img-thumbnail border-0" />
                   
                 </div>
                 <div class="pl-3 pt-2 pr-2 pb-2 w-75 border-left">
